a27d2fe99dd7c67c94e0e1d8536100352d1eeeec
Kyle Graniten <capstonedev2@gmail.com>
2026-05-28 15:07:31 +0800

    Fix Vercel Prisma build import

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -5,6 +5,8 @@ const globalForPrisma = globalThis as unknown as {
   prisma?: PrismaClient;
 };
 
+let cachedPrisma: PrismaClient | undefined;
+
 function getPoolMax() {
   const parsed = Number.parseInt(process.env.DATABASE_POOL_MAX ?? '', 10);
 
@@ -38,8 +40,39 @@ function createPrismaClient() {
   });
 }
 
-export const prisma = globalForPrisma.prisma ?? createPrismaClient();
+function getPrismaClient() {
+  const existingClient = globalForPrisma.prisma ?? cachedPrisma;
 
-if (process.env.NODE_ENV !== 'production') {
-  globalForPrisma.prisma = prisma;
+  if (existingClient) {
+    return existingClient;
+  }
+
+  const client = createPrismaClient();
+  cachedPrisma = client;
+
+  if (process.env.NODE_ENV !== 'production') {
+    globalForPrisma.prisma = client;
+  }
+
+  return client;
 }
+
+export const prisma = new Proxy({} as PrismaClient, {
+  get(_target, property) {
+    const client = getPrismaClient();
+    const value = Reflect.get(client, property, client);
+    return typeof value === 'function' ? value.bind(client) : value;
+  },
+  set(_target, property, value) {
+    return Reflect.set(getPrismaClient(), property, value);
+  },
+  has(_target, property) {
+    return property in getPrismaClient();
+  },
+  ownKeys() {
+    return Reflect.ownKeys(getPrismaClient());
+  },
+  getOwnPropertyDescriptor(_target, property) {
+    return Reflect.getOwnPropertyDescriptor(getPrismaClient(), property);
+  }
+}) as PrismaClient;
